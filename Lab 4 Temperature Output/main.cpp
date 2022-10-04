ba5001ab4332d721ed999242682c354bb678e873
Cacree21 <111894319+Cacree21@users.noreply.github.com>
2022-10-04 10:38:38 -0500

    Initial Commit

---
+++ Lab 4 Temperature Output/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Lab 4 Temperature Output
+//
+//  Created by Caleb Acree on 10/4/22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
